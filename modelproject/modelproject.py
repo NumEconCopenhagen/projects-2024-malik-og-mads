c436c34f12ad7aa2e33368d10fe0a0c1fbb9d5bd
vmf459 <vmf459@alumni.ku.dk>
2024-04-30 17:07:14 +0200

    xxx

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,22 +1,65 @@
+# Importing the packages we need
+from types import SimpleNamespace
 from scipy import optimize
+import numpy as np
+import sympy as sm
+from ipywidgets import interact
+import pandas as pd 
+import matplotlib.pyplot as plt
+import ipywidgets as widgets
+from IPython.display import display
 
-def solve_ss(alpha, c):
-    """ Example function. Solve for steady state k. 
+class Solow:
+    def __init__(self):
 
-    Args:
-        c (float): costs
-        alpha (float): parameter
+        # Setting up parameters
+        # Defining namespaces
+        par = self.par = SimpleNamespace()
+        sim = self.sim = SimpleNamespace()
 
-    Returns:
-        result (RootResults): the solution represented as a RootResults object.
+        # We start by setting up our algebraic parameters
+        par.K = sm.symbols('k')
+        par.A = sm.symbols('a')
+        par.L = sm.symbols('L')
+        par.Y = sm.symbols('Y')
+        par.S = sm.symbols('S')
+        
+        par.alpha = sm.symbols('alpha')
+        par.delta = sm.symbols('delta')
+        par.s = sm.symbols('s')
+        par.g = sm.symbols('g')
+        par.n = sm.symbols('n')
 
-    """ 
+        # We define our capital per effective worker
+        par.k = sm.symbols('k')
+
+        # Defining our sim variables
+        sim.alpha = 0.3
+        sim.delta = 0.05
+        sim.s = 0.2  
+        sim.g = 0.2
+        sim.n = 0.02
+        self.num_periods = 100
+        self.k_0 = 1e-7
+
+    def ss_equation(self):
+        par = self.par
+        # We define our transition equation
+        f = par.k**par.alpha
+        tranisition = sm.Eq(par.k, 1/((1+par.n)*(1+par.g))*((1-par.delta)*par.k + par.s*f))
+
+        # We solve for the steady state
+        steady_state = sm.solve(tranisition, par.k)[0]
+
+        return steady_state
     
-    # a. Objective function, depends on k (endogenous) and c (exogenous).
-    f = lambda k: k**alpha - c
-    obj = lambda kss: kss - f(kss)
+    def ss_value(self): 
 
-    #. b. call root finder to find kss.
-    result = optimize.root_scalar(obj,bracket=[0.1,100],method='bisect')
+        par = self.par
+        sim = self.sim
+
+        # We turn our symbolic steady state into a function
+        ss_function = sm.lambdify((par.s,par.g,par.n,par.delta,par.alpha), self.ss_equation())
+
+        return ss_function(sim.s,sim.g,sim.n,sim.delta,sim.alpha)
     
-    return result
