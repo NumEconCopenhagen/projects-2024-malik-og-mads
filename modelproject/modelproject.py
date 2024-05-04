472499aa80cdb33e3894399781c999ba4cfe0aed
mfk601 <mfk601@alumni.ku.dk>
2024-05-04 13:28:38 +0200

    Update

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -63,3 +63,46 @@ class Solow:
 
         return ss_function(sim.s,sim.g,sim.n,sim.delta,sim.alpha)
     
+    def transition_diagram(self):
+
+        capital_values = [self.k_0]  # List to store the capital stock values
+
+        # Calculate the capital stock values for each period
+        for _ in range(self.num_periods):
+            k_t = capital_values[-1]  # Current capital stock
+            k_t1 = 1 / ((1 + self.sim.n) * (1 + self.sim.g)) * (
+                    self.sim.s * k_t ** self.sim.alpha + (1 - self.sim.delta) * k_t)
+            capital_values.append(k_t1)
+
+        # Transition diagram plotting
+        plt.figure(figsize=(10, 6)) 
+        plt.plot(capital_values[:-1], capital_values[1:], 'bo-', markersize=0, linewidth=1, label='k_t+1')
+        plt.plot(capital_values[:-1], capital_values[:-1], 'ro-', markersize=0, linewidth=1, label='k_t = k_t+1')
+        plt.xlabel('Capital level at t')
+        plt.ylabel('Capital level at t+1')
+        plt.title('Transition diagram of capital')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    def interactive_plot(self):
+        # FloatSliders for simulation variables
+        a_slider = widgets.FloatSlider(min=0, max=0.5, step=0.01, value=0.3, description='Alpha')
+        d_slider = widgets.FloatSlider(min=0, max=0.5, step=0.01, value=0.05, description='Delta')
+        n_slider = widgets.FloatSlider(min=0, max=0.5, step=0.01, value=0.02, description='n')
+        g_slider = widgets.FloatSlider(min=0, max=0.5, step=0.01, value=0.2, description='g')
+        s_slider = widgets.FloatSlider(min=0, max=0.5, step=0.01, value=0.2, description='s')
+
+
+        # Interactive update of simulation variables
+        def update_sliders(alpha, delta, n, g, s):
+            self.sim.alpha = alpha
+            self.sim.delta = delta
+            self.sim.n = n
+            self.sim.g = g
+            self.sim.s = s
+            self.transition_diagram()
+
+        interactive_plot = widgets.interactive(update_sliders,
+                                               alpha=a_slider, delta=d_slider, n=n_slider, g=g_slider, s=s_slider)
+        display(interactive_plot)    
